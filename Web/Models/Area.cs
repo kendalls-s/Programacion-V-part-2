7af0ced1f22db0cf80b6772e5aef7e1cd5b1b135
Maddy <corderoruizmadelinee@gmail.com>
2026-07-22 20:03:42 -0600

    ENTITIES AREAS Y CARRERAS

---
+++ Web/Models/Area.cs
@@ -0,0 +1,12 @@
+﻿namespace SRV4_Areas.Entities;
+
+public class AreaTrabajo
+{
+    public int ID { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public int InstitucionID { get; set; }
+    public string InstitucionNombre { get; set; } = string.Empty;
+    public bool Activo { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public DateTime? FechaModificacion { get; set; }
+}

88a92f0d33ebf99b5156846d12f7da7e67dc4c30
vane1407ap-glitch <vane1407ap@gmail.com>
2026-07-22 22:47:45 -0600

    Delete Web/Services/IEmailService.cs

--- Web/Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SRV11_AutoRegistro.Services
-{
-    public interface IEmailService
-    {
-        Task EnviarCorreoConfirmacionAsync(
-            string destino,
-            string enlaceConfirmacion);
-    }
-}

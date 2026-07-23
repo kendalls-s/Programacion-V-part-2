358ca671f97ec6dab04ffc325dfabdf484e25572
Maddy <corderoruizmadelinee@gmail.com>
2026-07-22 20:05:46 -0600

    ENTITIES BITACORA Y AUTOREGISTRO

---
+++ Web/Models/Bitacora.cs
@@ -0,0 +1,11 @@
+﻿namespace BitacoraSRV9.Entities;
+
+public class Bitacora
+{
+    public int Id { get; set; }
+    public string Usuario { get; set; } = string.Empty;
+    public string Accion { get; set; } = string.Empty;
+    public string? DetalleJson { get; set; }
+    public bool EsError { get; set; } = false; 
+    public DateTime Fecha { get; set; }
+}
